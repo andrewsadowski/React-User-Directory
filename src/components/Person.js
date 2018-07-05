445561c6fadc8b84e34d6bd4bfd757e78e84ce67
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-04 20:12:46 -0700

    deconstructed username off props and displayed via Person component

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Person extends Component {
   render(props) {
-    const { email, name } = this.props.details;
+    const { email, name, username } = this.props.details;
     return (
       <div className="personInfo-container">
         <p className="person-name">{name}</p>
+        <p className="person-username">{username}</p>
         <p className="person-email">{email}</p>
       </div>
     );
